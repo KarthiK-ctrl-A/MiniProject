af34d2b738ea5c984a9608f376ced269558a1446
Karthik-ctrl-A <saikarthik4me@gmail.com>
2021-01-15 09:54:08 +0530

    deploy new app

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Button } from "./Button";
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth<= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener("resize",showButton);
+    return (
+     <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo'>
+                    Being Engineer  
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                        <Link to="/Home" className="nav-links" onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Content" className="nav-links" onClick={closeMobileMenu}>
+                            Content
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                            SIGN UP
+                        </Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+            </div>
+        </nav>
+     </>
+    )
+}
+
+export default Navbar
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvGlgD_PoAUOXlp_MUErnFszWjhdz0U3I",
+    authDomain: "new1-7e833.firebaseapp.com",
+    databaseURL: "https://new1-7e833.firebaseio.com",
+    projectId: "new1-7e833",
+    storageBucket: "new1-7e833.appspot.com",
+    messagingSenderId: "282722640959",
+    appId: "1:282722640959:web:a5b8927b214d200d27448a"
+  };
